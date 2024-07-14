bcdacf2ef8167df7b81246472d7bc3dec5d04502
Shipyde <raecke@shipy.de>
2024-07-14 09:49:39 +0200

    Initial Commit

---
+++ SunTrack Forcast/SunTrack_ForcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunTrack_ForcastApp.swift
+//  SunTrack Forcast
+//
+//  Created by Nico Raecke on 14.07.24.
+//
+
+import SwiftUI
+
+@main
+struct SunTrack_ForcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
