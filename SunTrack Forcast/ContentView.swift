d91418932cc70425db5e97ee70383f8d1cd476e7
Shipy <34223024+Shipyde@users.noreply.github.com>
2024-07-15 19:26:09 +0200

    Create ContentView.swift

---
+++ SunTrack Forcast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SunTrack Forcast
+//
+//  Created by Nico Raecke on 14.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var model = ViewModel()
+    
+    var body: some View {
+        if model.hasEnteredUserValues == false {
+            EntryUserValuesView(model: model)
+        }else {
+            DashboardView(model: model)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
