22ea0a8ac9525e4a2dc28f8defb309e7fdd9d21e
Robert Krx <87395576+Murdoc1988@users.noreply.github.com>
2024-07-15 18:36:49 +0200

    --added Sheet --changed TopBar

---
+++ SunTrack Forcast/SheetUserValuesView.swift
@@ -0,0 +1,18 @@
+//
+//  SheetUserValuesView.swift
+//  SunTrack Forcast
+//
+//  Created by Robert Krautchick on 15.07.24.
+//
+
+import SwiftUI
+
+struct SheetUserValuesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetUserValuesView()
+}
